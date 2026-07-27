9fd6f53651a01439605b4f6817bc5d449d19f673
Zoe Dauphinee <zdauphinee@greatermidland.org>
2026-07-27 12:07:01 -0400

    drop in activities color mapping

--- js/schedule-data.js
+++ js/schedule-data.js
@@ -49,8 +49,10 @@ const ScheduleData = (() => {
 
   const PALETTE = {
     red:['bg-red-100','text-red-800','border-red-500'],
+    rose:['bg-rose-100','text-rose-800','border-rose-500'],
     orange:['bg-orange-100','text-orange-800','border-orange-500'],
     yellow:['bg-yellow-100','text-yellow-800','border-yellow-500'],
+    lime:['bg-lime-100','text-lime-800','border-lime-500'],
     green:['bg-green-100','text-green-800','border-green-500'],
     teal:['bg-teal-100','text-teal-800','border-teal-500'],
     cyan:['bg-cyan-100','text-cyan-800','border-cyan-500'],
@@ -62,8 +64,19 @@ const ScheduleData = (() => {
   };
 
   const ACTIVITY_COLORS = {
-    'LAP SWIM':'blue', 'REC SWIM':'green',
-    'BASKETBALL':'orange', 'VOLLEYBALL':'yellow', 'PICKLEBALL':'red',
+    /* Aquatics */
+    'LAP SWIM':'blue', 'REC SWIM':'green', 'OPEN SWIM':'cyan',
+
+    /* Court Sports */
+    'OPEN BASKETBALL':'rose', 'ADULT BASKETBALL':'orange', 'YOUTH BASKETBALL':'yellow', 'VOLLEYBALL':'lime', 'PICKLEBALL':'red',
+
+    /* Community */
+    'EUCHRE':'pink', 'LINE DANCING':'purple', 'MAH JONGG':'indigo',
+
+    /* Child Watch */
+    'CHILD WATCH':'teal',
+
+    /* Default */
     'DEFAULT':'gray',
   };
 
